744d5fea8b3d816e02d0ba66ed54e21000d628db
theharisgroupllc <theharisgroupllc@gmail.com>
2026-04-26 03:04:35 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
+  basePath: isProd ? '/My-Business-Site' : '',
+  assetPrefix: isProd ? '/My-Business-Site' : '',
   images: {
     remotePatterns: [
       {
